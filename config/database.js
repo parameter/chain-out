fc446b8226d950f85035c0547b6568784bb29f72
parameter <paer.henriksson@yahoo.com>
2025-09-30 13:47:57 +0200

    wtf 8

--- config/database.js
+++ config/database.js
@@ -46,6 +46,49 @@ const createCollections = async () => {
 };
 
 const createIndexes = async () => {
+
+  await db.collection('courses').updateMany(
+    {},
+    [
+      {
+        $set: {
+          _lat: { $convert: { input: "$geolocation.lat", to: "double", onError: null, onNull: null } },
+          _lng: { $convert: { input: "$geolocation.lng", to: "double", onError: null, onNull: null } }
+        }
+      },
+      {
+        $set: {
+          _lat: {
+            $cond: [
+              { $ne: ["$_lat", null] },
+              { $max: [-90, { $min: [90, "$_lat"] }] },
+              null
+            ]
+          },
+          _lng: {
+            $cond: [
+              { $ne: ["$_lng", null] },
+              { $max: [-180, { $min: [180, "$_lng"] }] },
+              null
+            ]
+          }
+        }
+      },
+      {
+        $set: {
+          location: {
+            $cond: [
+              { $and: [{ $ne: ["$_lat", null] }, { $ne: ["$_lng", null] }] },
+              { type: "Point", coordinates: ["$_lng", "$_lat"] },
+              "$$REMOVE"
+            ]
+          }
+        }
+      },
+      { $unset: ["_lat", "_lng"] }
+    ]
+  );
+
   try {
     // Create unique index on email
     await db.collection('users').createIndex({ email: 1 }, { unique: true });
