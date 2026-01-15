6744ffb06e164d72771666eb94fa24fac20e80c4
parameter <paer.henriksson@yahoo.com>
2026-01-15 07:50:21 +0100

    suggest-new-course route 006

--- courses-admin/src/components/CourseForm.js
+++ courses-admin/src/components/CourseForm.js
@@ -8,7 +8,7 @@ axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000
 function generateUUID() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     const r = Math.random() * 16 | 0;
-    const v = c === 'x' ? r : (r & 0x3 | 0x8);
+    const v = c === 'x' ? r : ((r & 0x3) | 0x8);
     return v.toString(16);
   });
 }
