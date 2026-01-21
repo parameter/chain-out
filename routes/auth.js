af1367e106584d3bae28d468ccb896dd034d8e7c
parameter <paer.henriksson@yahoo.com>
2026-01-21 21:35:50 +0100

    temp31

--- routes/auth.js
+++ routes/auth.js
@@ -74,17 +74,15 @@ router.post('/login', (req, res, next) => {
     }
     if (!user) {
       // Check if the error is due to unverified email
-
       /*
       if (info?.emailNotVerified) {
         return res.status(403).json({ 
           message: info.message || 'Please verify your email address before logging in.',
           emailNotVerified: true 
         });
-      }
+      } */
       return res.status(401).json({ message: info?.message || 'Invalid credentials' });
     }
-    */
 
     const token = jwt.sign(
       { sub: user._id, userType: user.userType },
