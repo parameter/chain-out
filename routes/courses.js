c55f80e8da16ec0a26b897c8a6caa37dda96d14d
parameter <paer.henriksson@yahoo.com>
2026-04-12 16:55:57 +0200

    courses api 10

--- routes/courses.js
+++ routes/courses.js
@@ -85,6 +85,10 @@ router.post('/update-course', requireAuth, async (req, res) => {
       return res.status(400).json({ message: 'Course id is required' });
     }
 
+
+    console.log('courseId', courseId);
+    console.log('req.user._id', req.user._id);
+
     // check if current user is the owner of the course in course-admins
     const db = getDatabase();
     const courseAdminsCollection = db.collection('course-admins');
