0248323e206f1a988aa3181418ae5557f4f86ad7
parameter <paer.henriksson@yahoo.com>
2026-05-11 10:07:27 +0200

    only won badges from that round on earnedBadges 20

--- routes/users.js
+++ routes/users.js
@@ -2313,6 +2313,8 @@ router.post('/scorecard/complete-round', requireAuth, async (req, res) => {
       new Set([...invitedIds, ...addedIds, ...creatorId].map(String))
     );
 
+    console.log('allUserIds', allUserIds);
+
     // Notify everyone involved with the scorecard (invited users, added players, and creator)
     try {
       await Promise.all(
