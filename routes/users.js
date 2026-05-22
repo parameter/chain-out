bf3ad7664e863866773cad2baa22bd3640b7cca1
parameter <paer.henriksson@yahoo.com>
2026-05-22 09:42:14 +0200

    testing some shit 13

--- routes/users.js
+++ routes/users.js
@@ -207,16 +207,15 @@ router.post('/settings', requireAuth, async (req, res) => {
     const slots = normalizeBraggingSlots(braggingSlots);
     slots[idx] = badgeId;
 
-    console.log('req.user._id', req.user._id);
-
     const userSettings = await userSettingsCollection.findOneAndUpdate(
-      { userId: new ObjectId(req.user._id) },
+      { userId: req.user._id },
       { $set: { braggingSlots: slots } },
-      { projection: { _id: 0, userId: 0 } },
-      { upsert: true, returnDocument: 'after' }
+      {
+        projection: { _id: 0, userId: 0 },
+        upsert: true,
+        returnDocument: 'after'
+      }
     );
-
-    console.log('updated userSettings', userSettings);
     
     res.json(userSettings);
   } catch (err) {
