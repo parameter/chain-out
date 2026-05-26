9fce70e979131cbddd19dc8273729506fa95a2dc
parameter <paer.henriksson@yahoo.com>
2026-05-26 11:03:43 +0200

    testing some shit 34

--- routes/users.js
+++ routes/users.js
@@ -2505,7 +2505,7 @@ const filterFreemium = ({ badges, lastDayOfPremium, tierCutoff }) => {
     .map((badge) => trimTierProgress(badge, premiumCutoff, tierCutoff))
     .filter((badge) => {
       if (Array.isArray(badge.tierProgress)) {
-        // return badge.tierProgress.length > 0;
+        return true;
       }
 
       console.log('badge.badgeId 1', badge.badgeId);
